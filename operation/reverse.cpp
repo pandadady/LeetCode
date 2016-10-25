d0dbc7f5ca89a041aeba9d8fe45eeaa3b1506f72
shiduo <675767685@qq.com>
2016-10-25 14:45:24 +0800

    Update reverse.cpp

--- operation/reverse.cpp
+++ operation/reverse.cpp
@@ -1,4 +1,9 @@
-ListNode* updown(ListNode* root) {
+	struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+	};
+	ListNode* updown(ListNode* root) {
 		ListNode* next = NULL;
 		ListNode* pre = NULL;
 		while (root) {
