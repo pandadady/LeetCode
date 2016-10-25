d34feae13ccc35827cf39ed341624fd0e18aaa71
shiduo <675767685@qq.com>
2016-10-25 15:08:38 +0800

    Update listnode.cpp

--- operation/listnode.cpp
+++ operation/listnode.cpp
@@ -1,4 +1,27 @@
-ListNode* fast_slow(ListNode* root) {
+	//链表结构
+	struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+	};
+	/*
+	功能：翻转链表
+	*/
+	ListNode* updown(ListNode* root) {
+		ListNode* next = NULL;
+		ListNode* pre = NULL;
+		while (root) {
+			next = root->next;
+			root->next = pre;
+			pre = root;
+			root = next;
+		}
+		return pre;
+	}	
+	/*
+	功能：返回链表中间节点
+	*/
+	ListNode* fast_slow(ListNode* root) {
 		if (!root)return NULL;
 		ListNode* fast = root;
 		ListNode* slow = root;
@@ -6,5 +29,5 @@ ListNode* fast_slow(ListNode* root) {
 			slow = slow->next;
 			fast = fast->next->next;
 		}
-		return slow;
+		return slow->next;
 	}
