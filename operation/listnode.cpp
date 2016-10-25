c9b6cbcf11104aa6a3c174aa5867ff68387ba6cd
shiduo <675767685@qq.com>
2016-10-25 15:22:13 +0800

    Update listnode.cpp

--- operation/listnode.cpp
+++ operation/listnode.cpp
@@ -31,3 +31,24 @@
 		}
 		return slow->next;
 	}
+	/*
+	功能：将2个有序链表合并
+	*/
+	ListNode* mergelist(ListNode* l1, ListNode* l2) {
+		ListNode dummpy(0);
+		ListNode* root = &dummpy;
+		while (l1&&l2) {
+			if (l1->val < l2->val) {
+				root->next = l1;
+				l1 = l1->next;
+			}
+			else {
+				root->next = l2;
+				l2 = l2->next;
+			}
+			root = root->next;
+		}
+		if (l1 && !l2) root->next = l1;
+		if (!l1 && l2) root->next = l2;
+		return dummpy.next;
+	}
