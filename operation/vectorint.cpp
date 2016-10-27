b7a85143b02f4cd1742dd4baf6e2894b929e913c
shiduo <675767685@qq.com>
2016-10-27 14:32:15 +0800

    Create vectorint.cpp

---
+++ operation/vectorint.cpp
@@ -0,0 +1,8 @@
+vector<int> v_updown(vector<int> &vec) {
+		for (int i = 0, j = vec.size() - 1; j >= (vec.size() - 1) / 2 && i <= (vec.size() - 1) / 2; i++,j--) {
+			int tmp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = tmp;
+		}
+		return vec;
+	}
