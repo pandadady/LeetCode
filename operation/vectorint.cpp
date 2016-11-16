726e872268071ef97997a7e84c51d71ad5f9c24f
shiduo <675767685@qq.com>
2016-11-16 14:55:47 +0800

    Update vectorint.cpp

--- operation/vectorint.cpp
+++ operation/vectorint.cpp
@@ -11,3 +11,13 @@
 		}
 		return vec;
 	}
+	/*
+	功能：双下标循环，返回数组中不相同元素个数，前提是数组已排序
+	*/
+	int same_direction(vector<int> &vec) {
+		int index = 0;
+		for (int i = 0; i <= vec.size()-1; i++) {
+			if (vec[i] != vec[index])vec[++index] = vec[i];
+		}
+		return ++index;
+	}
